98f54844c1efd310b3d3e8b981052be825616f71
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-11-13 23:19:20 +0100

    started twig

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -1,10 +1,11 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HelloController
+class HelloController extends AbstractController
 {
     private array $messages = [
         "Hello", "Hi", "Bye!"
@@ -20,7 +21,14 @@ class HelloController
     #[Route('/messages/{id<\d+>}', name: 'app_show_one')]
     public function showOne(int $id): Response
     {
-        return new Response('<b>$this->messages[$id]</b>');
+        return $this->render(
+            'hello/show_one.html.twig',
+            [
+                'message' => $this->messages[$id]
+            ]
+
+        );
+        // return new Response($this->messages[$id]);
 
 
     }
