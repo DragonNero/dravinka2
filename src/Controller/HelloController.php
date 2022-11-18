c47ef69f41a5dc58d80f076c1384410d74ed5f96
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-11-18 18:52:43 +0100

    used twig functions

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -8,8 +8,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HelloController extends AbstractController
 {
     private array $messages = [
-        "Hello", "Hi", "Bye!"
-    ];
+        ['message' => 'Hello', 'created' => '2022/11/12'],
+        ['message' => 'Hi', 'created' => '2022/10/12'],
+        ['message' => 'Bye!', 'created' => '2021/08/12']
+      ];
+
     #[Route('/{limit<\d+>?3}', name: 'app_index')]
     public function index(int $limit): Response
     {
