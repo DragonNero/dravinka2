00de1bd28218fc68947f67ffd2de25a904a5c797
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-12-21 23:51:04 +0100

    added user ban

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,18 +2,16 @@
 
 namespace App\Controller;
 
-use DateTime;
 use App\Entity\Comment;
 use App\Entity\MicroPost;
 use App\Form\CommentType;
 use App\Form\MicroPostType;
 use App\Repository\CommentRepository;
 use App\Repository\MicroPostRepository;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MicroPostController extends AbstractController
@@ -22,35 +20,45 @@ class MicroPostController extends AbstractController
     public function index(MicroPostRepository $posts): Response
     {
         return $this->render('micro_post/index.html.twig', [
-            'posts' => $posts ->findAllWithComments(),
+            'posts' => $posts->findAllWithComments(),
         ]);
     }
 
     #[Route('/micro-post/{post}', name: 'app_micro_post_show')]
+    #[IsGranted(MicroPost::VIEW, 'post')]
     public function showOne(MicroPost $post): Response
     {
         return $this->render('micro_post/show.html.twig', [
             'post' => $post,
         ]);
     }
-    #[Route('/micro-post/add', name: 'app_micro_post_add', priority:2 )]
 
+    #[Route(
+        '/micro-post/add',
+        name: 'app_micro_post_add',
+        priority: 2
+    )]
     #[IsGranted('ROLE_WRITER')]
-    public function save(Request $request, MicroPostRepository $posts
+    public function add(
+        Request $request,
+        MicroPostRepository $posts
     ): Response {
-
-        $form = $this->createForm(MicroPostType::class, new MicroPost());
-
+        $form = $this->createForm(
+            MicroPostType::class,
+            new MicroPost()
+        );
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid())
-        {
+        if ($form->isSubmitted() && $form->isValid()) {
             $post = $form->getData();
             $post->setAuthor($this->getUser());
-            $posts->save($post, true);
+            $posts->add($post, true);
 
-            $this->addFlash('success', 'Your micro post have been added' );
+            // Add a flash
+            $this->addFlash('success', 'Your micro post have been addded.');
+
             return $this->redirectToRoute('app_micro_post');
+            // Redirect
         }
 
         return $this->renderForm(
@@ -62,20 +70,24 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('/micro-post/{post}/edit', name: 'app_micro_post_edit')]
-    #[IsGranted('ROLE_EDITOR')]
-    public function edit(MicroPost $post, Request $request, MicroPostRepository $posts): Response
-    {
-        
-        $form = $this ->createForm(MicroPostType::class, $post);
+    #[IsGranted(MicroPost::EDIT, 'post')]
+    public function edit(
+        MicroPost $post,
+        Request $request,
+        MicroPostRepository $posts
+    ): Response {
+        $form = $this->createForm(MicroPostType::class, $post);
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid())
-        {
+        if ($form->isSubmitted() && $form->isValid()) {
             $post = $form->getData();
-            $posts->save($post, true);
+            $posts->add($post, true);
 
-            $this->addFlash('success', 'Your micro post have been updated' );
+            // Add a flash
+            $this->addFlash('success', 'Your micro post have been updated.');
+
             return $this->redirectToRoute('app_micro_post');
+            // Redirect
         }
 
         return $this->renderForm(
@@ -89,23 +101,28 @@ class MicroPostController extends AbstractController
 
     #[Route('/micro-post/{post}/comment', name: 'app_micro_post_comment')]
     #[IsGranted('ROLE_COMMENTER')]
-    public function addComment(MicroPost $post, Request $request,  CommentRepository $comments): Response
-    {
-        
-        $form = $this ->createForm(CommentType::class, new Comment());
+    public function addComment(
+        MicroPost $post,
+        Request $request,
+        CommentRepository $comments
+    ): Response {
+        $form = $this->createForm(CommentType::class, new Comment());
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid())
-        {
+        if ($form->isSubmitted() && $form->isValid()) {
             $comment = $form->getData();
             $comment->setPost($post);
             $comment->setAuthor($this->getUser());
-            $comments->save($comment, true);
+            $comments->add($comment, true);
 
-            $this->addFlash('success', 'Your comment has been updated' );
-            return $this->redirectToRoute('app_micro_post_show', 
+            // Add a flash
+            $this->addFlash('success', 'Your comment have been updated.');
+
+            return $this->redirectToRoute(
+                'app_micro_post_show',
                 ['post' => $post->getId()]
             );
+            // Redirect
         }
 
         return $this->renderForm(
