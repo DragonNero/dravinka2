8af1584ac8c5c89d50c96625b787bcae7e9289b0
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-12-23 23:27:02 +0100

    added like functionality

--- src/Controller/LikeController.php
+++ src/Controller/LikeController.php
@@ -2,17 +2,41 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\MicroPost;
+use App\Repository\MicroPostRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class LikeController extends AbstractController
 {
-    #[Route('/like', name: 'app_like')]
-    public function index(): Response
-    {
-        return $this->render('like/index.html.twig', [
-            'controller_name' => 'LikeController',
-        ]);
+    #[Route('/like/{id}', name: 'app_like')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function like(
+        MicroPost $post,
+        MicroPostRepository $posts,
+        Request $request
+    ): Response {
+        $currentUser = $this->getUser();
+        $post->addLikedBy($currentUser);
+        $posts->save($post, true);
+
+        return $this->redirect($request->headers->get('referer'));
+    }
+
+    #[Route('/unlike/{id}', name: 'app_unlike')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function unlike(
+        MicroPost $post,
+        MicroPostRepository $posts,
+        Request $request
+    ): Response {
+        $currentUser = $this->getUser();
+        $post->removeLikedBy($currentUser);
+        $posts->save($post, true);
+
+        return $this->redirect($request->headers->get('referer'));
     }
 }
